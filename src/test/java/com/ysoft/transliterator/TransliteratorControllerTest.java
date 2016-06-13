8323dccd66eb2e9215e68d9e73f1007ac23546b2
ferry2 <ferry240@gmail.com>
2016-06-13 17:27:29 +0300

    Setup for spring controller unit testing

---
+++ src/test/java/com/ysoft/transliterator/TransliteratorControllerTest.java
@@ -0,0 +1,27 @@
+package com.ysoft.transliterator;
+
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = { WebConfig.class })
+@WebAppConfiguration
+public class TransliteratorControllerTest {
+
+	private MockMvc mockMvc;
+	
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+	
+	@Before
+	public void setUp() {
+		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+	}
+}
