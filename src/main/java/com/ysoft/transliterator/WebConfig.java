a07f743ebf2b88177ccb1ee3df66bd5b073ba2dc
ferry2 <ferry240@gmail.com>
2016-06-14 11:31:18 +0300

    Added test for TransliteratorController. servlet-api version had to be changed to 3.1.0 in order tests to pass.

--- src/main/java/com/ysoft/transliterator/WebConfig.java
+++ src/main/java/com/ysoft/transliterator/WebConfig.java
@@ -1,16 +1,37 @@
 package com.ysoft.transliterator;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
 @EnableWebMvc
 public class WebConfig extends WebMvcConfigurerAdapter {
 
 	@Override
-	  public void addResourceHandlers(ResourceHandlerRegistry registry) {
-	    registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
-	  }
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+	  registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+	}
+	
+	@Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+	  
+	@Bean
+	public ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+ 
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+ 
+        return viewResolver;
+    }
 }
--- src/test/java/com/ysoft/transliterator/TransliteratorControllerTest.java
+++ src/test/java/com/ysoft/transliterator/TransliteratorControllerTest.java
@@ -1,6 +1,10 @@
 package com.ysoft.transliterator;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
@@ -23,5 +27,11 @@ public class TransliteratorControllerTest {
 	@Before
 	public void setUp() {
 		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+	}
+
+	@Test
+	public void showHomePageTest() throws Exception {
+		mockMvc.perform(get("/transliterator"))
+			   .andExpect(status().isOk());
 	}
 }
